0a558ad7c450d212e2680cb829b69b21e08dfbf5
MountainAndMorning <2457065285@qq.com>
2024-08-14 23:05:54 +0800

    用vitepress内容替换原来的主页内容

---
+++ docs/.vitepress/config.mjs
@@ -0,0 +1,64 @@
+import { defineConfig } from 'vitepress'
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  title: "TIMESlice",
+  vite: {     // <=== insert this section
+    ssr: {
+      noExternal: ["vuetify"]
+    }
+  },
+  description: "医学影像管理与分析平台",
+  themeConfig: {
+    // https://vitepress.dev/reference/default-theme-config
+    logo: '/images/icon.png',
+    footer: {
+      message: 'Powered by Key Laboratory of Cardiovascular Imaging and Research of Liaoning Province, CHINA.'
+    },
+    nav: [
+      { text: '主页', link: '/' },
+      { text: '下载', link: '/download' }
+    ],
+
+    sidebar: [
+      {
+        text: '简介', link: '/manual/index',
+      },
+      {
+        text: '软件激活', link: '/manual/activation',
+      },
+      {
+        text: '数据管理',
+        items: [
+          { text: '数据库管理', link: '/manual/database' },
+          { text: '数据结构', link: '/manual/dataManage' },
+          { text: '影像导入', link: '/manual/importScan' },
+          { text: '表格导入', link: '/manual/importTable' },
+          { text: '标签管理', link: '/manual/labelManage' }
+        ]
+      },
+      {
+        text: '动物园',
+        link: '/manual/model',
+        items: [
+          { text: '图像分割', link: '/manual/pericardiumModel' },
+          { text: '冠脉中心线追踪', link: '/manual/centerlineModel' },
+          { text: '冠脉轮廓识别', link: '/manual/contourModel' }
+        ]
+      },
+      {
+        text: '图像分割',
+        link: '/segmentation/segmentation',
+        items: [
+          { text: 'seg管理', link: '/segmentation/manageSegmentation' },
+          { text: 'seg编辑', link: '/segmentation/editSegmentation' },
+        ]
+      },
+      { text: '下载', link: '/download' }
+    ],
+
+    socialLinks: [
+      // { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
+    ]
+  }
+})
---
+++ docs/index.md
@@ -0,0 +1,28 @@
+---
+# https://vitepress.dev/reference/default-theme-home-page
+layout: home
+
+hero:
+  name: "TIMESlice"
+  text: "医学影像管理与分析平台"
+  tagline: v4.16.0 
+  image:
+    src: /images/icon.png
+    alt: VitePress
+  actions:
+    - theme: brand
+      text: 手册
+      link: /manual/index
+    - theme: alt
+      text: 下载
+      link: /download
+
+features:
+  - title: 自动
+    details: 
+  - title: 简洁
+    details: 
+  - title: 强大
+    details: 
+---
+
